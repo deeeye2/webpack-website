41d491db0c5f7811ace223d1158b13532045216d
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-12-27 17:54:14 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import './style.css';
 document.addEventListener('DOMContentLoaded', () => {
     const appDiv = document.getElementById('app');
     appDiv.textContent = 'Hello, Parcel!';
+    appDiv.className = 'app';
 });
