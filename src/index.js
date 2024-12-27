3d00b75a9a15c479223a83b2195052604589f53a
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-12-27 17:44:33 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,5 @@ import './style.css';
 
 document.addEventListener('DOMContentLoaded', () => {
     const appDiv = document.getElementById('app');
-    if (appDiv) {
-        appDiv.textContent = 'Hello, Webpack!';
-        appDiv.className = 'app';
-    } else {
-        console.error('App div not found');
-    }
+    appDiv.textContent = 'Hello, Parcel!';
 });
-
